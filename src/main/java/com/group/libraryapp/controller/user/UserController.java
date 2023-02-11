77509f52581f8706be95503be258e931203c80c7
Baamki <sevenbumki@gmail.com>
2023-02-11 21:41:36 +0900

    CRUD API 리팩토링

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -4,6 +4,8 @@ import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
+import com.group.libraryapp.repository.user.UserRepository;
+import com.group.libraryapp.service.user.UserService;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.*;
@@ -17,57 +19,35 @@ import java.util.List;
 public class UserController {
 
 
-    private final JdbcTemplate jdbcTemplate;
+    private final UserService userService;
 
-    public UserController(JdbcTemplate jdbcTemplate){
-        this.jdbcTemplate = jdbcTemplate;
+    public UserController(UserService userService){
+        this.userService = userService;
     }
+
 
     @PostMapping("/user") //Post /User
     public void saveUser(@RequestBody UserCreateRequest userCreateRequest)
     {
-        String sql = "insert into users (name, age) values (?,?)";
-        jdbcTemplate.update(sql,userCreateRequest.getName(),userCreateRequest.getAge());
+        userService.saveUser(userCreateRequest);
     }
 
     @GetMapping("/user")
     public List<UserResponse> getUsers() {
-        String sql = "Select * from users";
-        return jdbcTemplate.query(sql, (rs, rowNum) -> {
-            long id = rs.getLong("id");
-            String name = rs.getString("name");
-            int age = rs.getInt("age");
-            return new UserResponse(id, name, age);
-        });
+        return userService.getUsers();
     }
 
 
     @PutMapping("/user")
     public void updateUser(@RequestBody UserUpdateRequest userUpdateRequest)
     {
-        String readSql = "Select * From users where id = ? ";
-        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, userUpdateRequest.getId()).isEmpty();
-        if (isUserNotExist)
-        {
-            throw new IllegalArgumentException();
-        }
-
-        String sql = "update users set name = ? where id = ? ";
-        jdbcTemplate.update(sql,userUpdateRequest.getName(), userUpdateRequest.getId());
+            userService.updateUser(userUpdateRequest);
     }
 
     @DeleteMapping("/user")
     public void deleteUser(@RequestParam String name)
     {
-        String readSql = "Select * From users where name = ? ";
-        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0,name).isEmpty();
-        if (isUserNotExist)
-        {
-            throw new IllegalArgumentException();
-        }
-
-        String sql ="delete from users where name = ? ";
-        jdbcTemplate.update(sql, name);
+        userService.deleteUser(name);
     }
 
     @GetMapping("/user/error-test")
---
+++ src/main/java/com/group/libraryapp/repository/user/UserRepository.java
@@ -0,0 +1,65 @@
+package com.group.libraryapp.repository.user;
+
+import com.group.libraryapp.dto.user.response.UserResponse;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class UserRepository {
+
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserRepository(JdbcTemplate jdbcTemplate)
+    {
+
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+    public boolean isUserNotExist(long userId)
+    {
+        String readSql = "Select * From users where id = ? ";
+        return jdbcTemplate.query(readSql, (rs, rowNum) -> 0, userId).isEmpty();
+    }
+
+    public void updateUserName(String name, long id)
+    {
+        String sql = "update users set name = ? where id = ? ";
+        jdbcTemplate.update(sql,name, id);
+    }
+
+    public boolean isUserNotExist(String name)
+    {
+        String readSql = "select * from users where name = ?";
+        return jdbcTemplate.query(readSql,(rs, rowNum) -> 0,name).isEmpty();
+    }
+
+    public void deleteUser(String name)
+    {
+        String sql = "Delete from users where name = ?";
+        jdbcTemplate.update(sql,name);
+    }
+
+
+    public void saveUser(String name, Integer age)
+    {
+        String sql = "insert into users (name, age) values (?,?)";
+        jdbcTemplate.update(sql,name,age);
+    }
+
+
+    public List<UserResponse> getUsers()
+    {
+        String sql = "Select * from users";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            long id = rs.getLong("id");
+            String name = rs.getString("name");
+            int age = rs.getInt("age");
+            return new UserResponse(id, name, age);
+        });
+    }
+
+}
